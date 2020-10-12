dfc96e0ed2e3ce3b544bad2ce8e4ba07152054d5
Yanming Zhang <yzhan27@outlook.com>
2020-10-12 05:08:52 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Post />
+  </div>
+</template>
+
+<script>
+  import Post from './components/Post'
+
+export default {
+  name: 'App',
+  components: {
+    Post
+  }
+}
+</script>
+
+<style lang="sass">
+        body
+              margin: 0
+              padding: 0
+              background:
+                    color: #F3F3F3
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,60 @@
+<template>
+  <article>
+    <h1>
+      {{ title }}
+    </h1>
+    <p>
+      I am an individual who is quite passionate about Cannabis and Hemp. I am a hard worker who is quite proficient 
+      in research and article writing.Being detail oriented I will ensure that the work will be of good quality and 
+      given upon the agreed deadline.
+      <br><br>
+      I am an individual who is quite passionate about Cannabis and Hemp. I am a hard worker who is quite proficient 
+      in research and article writing.Being detail oriented I will ensure that the work will be of good quality and 
+      given upon the agreed deadline.
+      <br><br>
+      I am an individual who is quite passionate about Cannabis and Hemp. I am a hard worker who is quite proficient 
+      in research and article writing.Being detail oriented I will ensure that the work will be of good quality and 
+      given upon the agreed deadline.
+      <br><br>
+      I am an individual who is quite passionate about Cannabis and Hemp. I am a hard worker who is quite proficient 
+      in research and article writing.Being detail oriented I will ensure that the work will be of good quality and 
+      given upon the agreed deadline.
+      <br><br>
+      I am an individual who is quite passionate about Cannabis and Hemp. I am a hard worker who is quite proficient 
+      in research and article writing.Being detail oriented I will ensure that the work will be of good quality and 
+      given upon the agreed deadline.
+      <br><br>
+      I am an individual who is quite passionate about Cannabis and Hemp. I am a hard worker who is quite proficient 
+      in research and article writing.Being detail oriented I will ensure that the work will be of good quality and 
+      given upon the agreed deadline.
+      <br><br>
+      I am an individual who is quite passionate about Cannabis and Hemp. I am a hard worker who is quite proficient 
+      in research and article writing.Being detail oriented I will ensure that the work will be of good quality and 
+      given upon the agreed deadline.
+      <br><br>
+      I am an individual who is quite passionate about Cannabis and Hemp. I am a hard worker who is quite proficient 
+      in research and article writing.Being detail oriented I will ensure that the work will be of good quality and 
+      given upon the agreed deadline.
+      <br><br>
+      I am an individual who is quite passionate about Cannabis and Hemp. I am a hard worker who is quite proficient 
+      in research and article writing.Being detail oriented I will ensure that the work will be of good quality and 
+      given upon the agreed deadline.
+      <br><br>     
+    </p>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "Post"
+}
+</script>
+
+<style lang="sass" scoped>
+  article
+    width: 100%
+    max-width: 768px
+    margin: 0 auto
+    background:
+      color: #FFFFFF
+</style>
